d6c4896f6250713218b4bf41beb2912f3d41db03
Andreas Wänqvist <andrkarl@gmail.com>
2024-07-27 11:40:38 +0200

    2

--- src/backend/SKDemo.Agents/RefineDraft2.cs
+++ src/backend/SKDemo.Agents/RefineDraft2.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace SKDemo.Agents
 {
-    internal class RefineDraft2
+    class RefineDraft2
     {
+
+        public RefineDraft2()
+        {
+            var builder = Kernel.CreateBuilder();
+
+            // using OpenAI
+            var openAIKey = Environment.GetEnvironmentVariable("OpenAIKey");
+            Debug.Assert(!string.IsNullOrEmpty(openAIKey), "OpenAIKey environment variable is not set.");
+
+            builder.AddOpenAIChatCompletion(
+                     "gpt-4o-mini",             // OpenAI Model name
+                     openAIKey);                // OpenAI API Key
+
+
+            var kernel = builder.Build();
+            kernel.ImportPluginFromType<MarkdownToHTML>();
+
+        }
     }
+
+    public sealed class MarkdownToHTML
+    {
+        [KernelFunction, Description("Convert markdown to HTML")]
+        public static string Convert([Description("Markdown text to convert")] string markdown) =>
+            new MarkdownSharp.Markdown().Transform(markdown);
+    }
+
 }
